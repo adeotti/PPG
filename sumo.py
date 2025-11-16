6ef8e4eafa6e0b455a252bb28b7222115e137ac7
Wafick <wafickachiambi@gmail.com>
2025-11-16 12:03:17 -0500

    Update

--- sumo.py
+++ sumo.py
@@ -13,13 +13,17 @@ class Hypers:
 
 hypers = Hypers()
 
+e = gym.make("sudoku-v0")
+x = e.reset()[0]
+
 def env():
     ...
 
-def process_obs(x):
-    x = torch.tensor(x,dtype=torch.float32,device=hypers.device)
-    m = (x == 0).to(torch.float32)
-    return torch.stack([x,m],dim=0)
+def process_obs(x): # - > one hot encoding + mask
+    x = torch.tensor(x,dtype=torch.int64,device=hypers.device)
+    m = (x == 0).unsqueeze(0).to(torch.float32)
+    x = F.one_hot(x,num_classes=10).permute(-1,0,1).float()
+    return torch.cat([x,m],dim=0).requires_grad_(True) 
 
 @torch.no_grad()
 def w_init(l):
@@ -30,29 +34,31 @@ def w_init(l):
 class network(nn.Module):
     def __init__(self):
         super().__init__()
-        self.c1 = nn.Conv2d()
-        self.c2 = nn.Conv2d()
-        self.c3 = nn.Conv2d()
+        self.c1 = nn.LazyConv2d(64,1,1)
+        self.c2 = nn.LazyConv2d(128,3,1)
+        self.c3 = nn.LazyConv2d(128,3,1)
     
-        self.l1 = nn.LazyLinear()
-        self.l2 = nn.LazyLinear()
-        self.l3 = nn.LazyLinear()
+        self.l1 = nn.LazyLinear(512)
+        self.l2 = nn.LazyLinear(256)
+        self.l3 = nn.LazyLinear(128)
 
-        self.p_head = nn.LazyLinear() # policy head
-        self.v_head = nn.LazyLinear() # value head
+        self.p_head = nn.LazyLinear(30) # policy head
+        self.v_head = nn.LazyLinear(1) # value head
     
     def forward(self,x): 
         x = self.c1(x)
         x = F.relu(self.c2(x))
-        x = F.relu(self.c3(x))
-
-        x = F.relu(self.l1(x))
+        x = F.relu(self.c3(x)) 
+        x = F.relu(self.l1(x.flatten(start_dim=1)))
         x = F.relu(self.l2(x))
         x = F.relu(self.l3(x))
 
         p = self.p_head(x) 
         v = self.v_head(x)
         
-        ...
+        return p  
 
+
+n = network()
+print(n(process_obs(x).unsqueeze(0)).shape)
 
