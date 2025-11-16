e1b39b2b5e1ec46528f2a0ba17dc0891123036c6
Wafick <wafickachiambi@gmail.com>
2025-11-15 19:55:26 -0500

    Update

--- sumo.py
+++ sumo.py
@@ -13,19 +13,21 @@ class Hypers:
 
 hypers = Hypers()
 
-env = gym.make("sudoku-v0")
+def env():
+    ...
 
 def process_obs(x):
     x = torch.tensor(x,dtype=torch.float32,device=hypers.device)
     m = (x == 0).to(torch.float32)
     return torch.stack([x,m],dim=0)
 
+@torch.no_grad()
 def w_init(l):
     if isinstance(l,(nn.Conv2d,nn.Linear)):
         nn.init.orthogonal_(l.weight)
         l.bias.fill_(0.0)
 
-class policy_n(nn.Module):
+class network(nn.Module):
     def __init__(self):
         super().__init__()
         self.c1 = nn.Conv2d()
@@ -50,6 +52,7 @@ class policy_n(nn.Module):
 
         p = self.p_head(x) 
         v = self.v_head(x)
-        return x
+        
+        ...
 
 
