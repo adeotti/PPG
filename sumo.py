20466d7cdece20675bee1cbad8a67aabbd9441f6
Wafick <wafickachiambi@gmail.com>
2025-11-23 22:33:13 -0500

    Add update method to update the vtarget preallocated space in the replay buffer

--- sumo.py
+++ sumo.py
@@ -106,7 +106,8 @@ class memory: # Replay buffer class
         self.state = torch.empty((B,N,9,9),device=hypers.device,dtype=torch.half)
         self.action = torch.empty((B,3,N),device=hypers.device,dtype=torch.float32)
         self.values = torch.empty((B,N,1),device=hypers.device,dtype=torch.float32)
-        self.values_aux = torch.empty((B,N,1),device=hypers.device,dtype=torch.float32) 
+        self.values_aux = torch.empty((B,N,1),device=hypers.device,dtype=torch.float32)
+        self.v_target = torch.empty((B,N,1),device=hypers.device,dtype=torch.float32)
         self.prob = torch.empty((B,N,3),device=hypers.device,dtype=torch.float32) 
         self.rewards = torch.empty((B,N),device=hypers.device,dtype=torch.float32) 
         self.dones = torch.empty((B,N),device=hypers.device,dtype=torch.float32) 
@@ -175,6 +176,7 @@ class memory: # Replay buffer class
             self.action[idx],
             self.values[idx].flatten(0,1),
             self.values_aux[idx],
+            self.v_target[idx].flatten(0,1),
             self.prob[idx],
             self.advantages[idx],
             self.dist_prob[idx]
@@ -183,7 +185,11 @@ class memory: # Replay buffer class
     def update_prob(self,x): # update (replace) the entire probability distribution
         x = x.reshape(*self.dist_prob.shape) 
         self.dist_prob = x
-       
+
+    def update_v_target(self,x): # update v target preallocated space 
+        x = x.reshape(*self.v_target.shape) 
+        self.v_target = x
+ 
     def traj_reward(self):
         return list(map(torch.tensor,(self.finished_reward,self.log_total_steps)))
 
@@ -221,7 +227,7 @@ class main:
         torch.save(data,f"./model-{n}")
 
     def process_sample(self): # sample and process parts of the sample
-        states,actions,values,v_policy,probs,advantages,dist_prob = self.memory.sample(hypers.minibatch)
+        states,actions,values,v_policy,v_target,probs,advantages,dist_prob = self.memory.sample(hypers.minibatch)
         actions = actions.transpose(1, 2).flatten(0,1)
         advantages = advantages.flatten().unsqueeze(-1)
         return (
@@ -229,6 +235,7 @@ class main:
             actions,
             values,
             v_policy,
+            v_target,
             probs,
             advantages,
             dist_prob
@@ -243,16 +250,15 @@ class main:
                 torch.compiler.cudagraph_mark_step_begin()
                 self.memory.compute_advantage()
                 frozen_probs = []
+                v_target_list = []
             
                 for _ in range(hypers.batchsize//hypers.minibatch):
-                    states,actions,values,_,probs,advantages,dist_prob = self.process_sample() 
+                    states,actions,values,_,_,probs,advantages,dist_prob = self.process_sample() 
                  
                     for r in range(hypers.optim_steps): # sample reuse N_pi = 32  
                         # -
                         p_out,_ = self.p_net(states) 
                         dist = Categorical(probs=p_out)
-                        if r == 0:
-                            frozen_probs.append(dist.probs)
                         new_probs = dist.log_prob(actions)
                         ratio = torch.exp(new_probs - probs.flatten(0,1)) 
                         p1 = ratio * advantages
@@ -260,22 +266,28 @@ class main:
                         loss_policy = - torch.mean(torch.min(p1,p2))
                         # -
                         new_values = self.v_net(states) 
-                        v_target = advantages + values 
+                        v_target = advantages + values       
                         loss_value = F.smooth_l1_loss(new_values.squeeze(), v_target)
                         # -
                         loss = loss_policy + loss_value - (hypers.beta * dist.entropy().mean())
                         self.optim.zero_grad(set_to_none=True)
                         loss.backward()
                         self.optim.step()
-      
+
+                        if r == 0: # data for the auxiliary phase
+                            frozen_probs.append(dist.probs)
+                            v_target_list.append(v_target)
+
                 frozen_probs = torch.stack(frozen_probs) 
+                v_targets = torch.stack(v_target_list) 
                 self.memory.update_prob(frozen_probs)
+                self.memory.update_v_target(v_targets) 
            
                 for _ in range(hypers.e_aux): # auxiliary phase 
                     for _ in range(hypers.batchsize//hypers.minibatch):   
-                        states,actions,values,v_policy,probs,advantages,dist_prob = self.process_sample()
+                        states,actions,values,v_policy,v_target,probs,advantages,dist_prob = self.process_sample()
 
-                        l_v_aux = F.smooth_l1_loss(v_policy,torch.stack(list_v_target))
+                        l_v_aux = F.smooth_l1_loss(v_policy,v_target)
                         l_joint = l_aux + (hypers.beta_clone * kl(dist_prob,torch.stack(list_new_dist_probs)).mean())
                                               
                         new_values = self.v_net(process_obs(states))
