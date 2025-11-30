da302660d5c4c71b69bc4005d93dff29cf51b09a
Wafick <wafickachiambi@gmail.com>
2025-11-29 21:25:41 -0500

    ...

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 from torch.distributions import Categorical
 
 def envi():
-    x = gym.make("sudoku-v0",render_mode="human")
+    x = gym.make("sudoku-v0",render_mode="human",horizon=500)
     return x 
 
 def process_obs(x): 
@@ -15,9 +15,7 @@ def process_obs(x):
 
 def softmax_mask(x): 
     x = x.reshape(x.shape[0],3,9) 
-    m = torch.zeros_like(x,dtype=torch.bool)  
-    m[:,0,-1] = True
-    m[:,1,-1] = True
+    m = torch.zeros_like(x,dtype=torch.bool)   
     m[:,-1,0] = True
     value = -float("inf")
     return torch.masked_fill(x,m,value)
@@ -28,36 +26,38 @@ class p_net(nn.Module):
         self.c1 = nn.LazyConv2d(64,1,1)
         self.c2 = nn.LazyConv2d(128,3,1)
         self.c3 = nn.LazyConv2d(128,3,1)
+        self.attn = nn.MultiheadAttention(128,4,batch_first=True)
         self.l1 = nn.LazyLinear(512)
         self.l2 = nn.LazyLinear(256)
         self.l3 = nn.LazyLinear(3*9)
-      
+        self.v_aux = nn.LazyLinear(1)    # auxiliary value head
+    
     def forward(self,x): 
         x = self.c1(x)
         x = F.relu(self.c2(x))
-        x = F.relu(self.c3(x)) 
-        x = F.relu(self.l1(x.flatten(start_dim=1)))
+        x = F.relu(self.c3(x))
+        x = x.flatten(2).transpose(-1,1)
+        x,attn_w = self.attn(x,x,x) 
+        x = F.relu(self.l1(x.flatten(1)))
         x = F.relu(self.l2(x))
-        x = self.l3(x) 
-        x = softmax_mask(x)
-        p_head = F.softmax(x,dim=-1)   
-        return p_head
+        x = F.relu(self.l3(x)) 
+        p_head = F.softmax(softmax_mask(x),dim=-1) # policy head output
+        v_aux = self.v_aux(x)                      # auxiliary value head output
+        return p_head,v_aux,attn_w
 
-model = p_net()
-model(process_obs(torch.randint(0,9,(1,9,9))))
-model.load_state_dict(torch.load("./model-10000",map_location="cpu"),strict=False)
+policy = p_net()
+policy(process_obs(torch.randint(0,9,(1,9,9))))
+policy.load_state_dict(torch.load("./model-22000",map_location="cpu"),strict=False)
 
 env = envi()
 obs = env.reset()[0]
 
 for n in range(2_000):
-    dist = model(process_obs(torch.tensor(obs,dtype=torch.int64).unsqueeze(0)))
-    action = Categorical(probs=dist).sample().squeeze()
-    obs,reward,_,_,_ = env.step(action.numpy())
+    dist,_,_ = policy(process_obs(torch.tensor(obs,dtype=torch.int64).unsqueeze(0)))
+    action = Categorical(probs=dist).sample().squeeze() 
+    obs,reward,done,_,_ = env.step(action.numpy())
     env.render()
-
-    if n>0 and n%300 == 0: 
-        env = envi()
+    if done:
         obs = env.reset()[0]
-        
-    
+
+
