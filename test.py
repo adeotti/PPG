b3367e47ea06b8f038cd0da5b9c102f487259f9d
Wafick <wafickachiambi@gmail.com>
2025-12-16 21:17:46 -0500

    Update policy net to be deterministic

--- test.py
+++ test.py
@@ -3,22 +3,16 @@ import gymnasium as gym
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.distributions import Categorical
+import numpy as np
 
 def envi():
-    x = gym.make("sudoku-v0",render_mode="human",horizon=500)
+    x = gym.make("sudoku-v0",render_mode="human",horizon=300)
     return x 
 
 def process_obs(x): 
-    m = (x == 0).unsqueeze(1).to(torch.float32)
+    m = (x == 0).unsqueeze(1).float()
     x = F.one_hot(x,num_classes=10).permute(0,-1,1,2).float() 
     return torch.cat([x,m],dim=1) 
-
-def softmax_mask(x): 
-    x = x.reshape(x.shape[0],3,9) 
-    m = torch.zeros_like(x,dtype=torch.bool)   
-    m[:,-1,0] = True
-    value = -float("inf")
-    return torch.masked_fill(x,m,value)
 
 class p_net(nn.Module):
     def __init__(self):
@@ -29,50 +23,58 @@ class p_net(nn.Module):
 
         self.emb = nn.Parameter(torch.zeros(1,81,128))
         self.attn = nn.MultiheadAttention(128,4,batch_first=True)
+        self.norm = nn.LayerNorm(128)
         self.l1 = nn.LazyLinear(128)
         self.l2 = nn.LazyLinear(128)
  
-        self.l3 = nn.LazyLinear(3*9)
-        self.v_aux = nn.LazyLinear(1)    # auxiliary value head
+        self.pos = nn.LazyLinear(1)
+        self.num = nn.LazyLinear(10)
+        self.v_aux = nn.LazyLinear(1) 
     
-    def forward(self,x): 
+    def forward(self,x):
         x = self.c1(x)
-        x = F.relu(self.c2(x))
-        x = F.relu(self.c3(x))
-        x = x.flatten(2).transpose(-1,1) # -> torch.Size([1,81,128]) 
-        
-        x = x + self.emb
-        x,a_w = self.attn(x,x,x) 
-        x = F.relu(self.l1(x))
-        x = F.relu(self.l2(x))
-        
-        x = F.relu(self.l3(x.mean(1))) 
-        p_head = F.softmax(self.cll_mask(x),dim=-1)  
-        v_aux = self.v_aux(x)                        
-        return p_head,v_aux,a_w
+        x = F.silu(self.c2(x))  
+        x = F.silu(self.c3(x))
+        x = x.flatten(2).transpose(-1,1) 
+        x = x + self.emb 
+        x,_ = self.attn(x,x,x)  
+        x = self.norm(x)
+        x = F.silu(self.l1(x))
+        x = F.silu(self.l2(x))
+        pos = self.pos(x).squeeze(-1)
+        pos = F.softmax(pos,-1)
+        pos = pos.argmax().item() 
+        num_logits = self.num(x)  
+        idx = torch.arange(x.size(0),device=x.device)
+        o = num_logits[idx,pos]
+        o = self.cll_mask(o)
+        o = F.softmax(o,-1)
+        num = o.argmax().item()                      
+        return pos,num
 
-    def cll_mask(self,x): # min(cell value) = 1 
-        x = x.reshape(x.shape[0],3,9) 
+    def cll_mask(self,x):
         m = torch.zeros_like(x,dtype=torch.bool)   
-        m[:,-1,0] = True
+        m[:,0] = True
         value = -float("inf")
         return torch.masked_fill(x,m,value)
 
 policy = p_net()
 policy(process_obs(torch.randint(0,9,(1,9,9))))
-policy.load_state_dict(torch.load("./model-10000",map_location="cpu"),strict=False)
+policy.load_state_dict(torch.load("./model-4000",map_location="cpu"),strict=False)
 
 env = envi()
 obs = env.reset()[0]
-
+r = 0
 for n in range(2_000):
-    dist,_,_ = policy(process_obs(torch.tensor(obs,dtype=torch.int64).unsqueeze(0)))
-    action = Categorical(probs=dist).sample().squeeze()
-    if action[-1] == 9:
-        sys.exit()
-    obs,reward,done,_,_ = env.step(action.numpy())
+    pos,num = policy(process_obs(torch.tensor(obs,dtype=torch.int64).unsqueeze(0)))
+    xpos = pos // 9 ; ypos = pos % 9
+    action = np.stack((xpos,ypos,num),axis=-1).reshape(3)
+    obs,reward,done,_,_ = env.step(action)
+    r+=reward
     env.render()
     if done:
+        print(r)
+        r = 0
         obs = env.reset()[0]
 
 
