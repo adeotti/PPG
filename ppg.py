77aa0cc11de05b97772ee844fc72a219edfef430
Adeotti <wafickachiambi@gmail.com>
2025-12-16 21:04:42 -0500

    Add policy and value nets

--- ppg.py
+++ ppg.py
@@ -11,6 +11,7 @@ import retro
 
 import numpy as np
 import gymnasium as gym
+from gymnasium.wrappers import GrayscaleObservation,ResizeObservation
 from gymnasium.vector import AsyncVectorEnv
 from dataclasses import dataclass
 from collections import deque
@@ -27,25 +28,52 @@ hypers = Hypers()
 
 def vec():
     def thunk():
-        env = retro.make(game="Airstriker-Genesis",render_mode=None)
-        # single env shape (224, 320, 3)
-        return env
+        e = retro.make(game="Airstriker-Genesis",render_mode=None) # single env shape (224, 320, 3)
+        e = ResizeObservation(e,(100,100))
+        e = GrayscaleObservation(e)
+        return e
     return AsyncVectorEnv([thunk for _ in range(hypers.num_envs)])
 
 def process_obs(x):
-    return torch.as_tensor(x,device=hypers.device).permute(0,-1,1,2)
+    return torch.as_tensor(x,device=hypers.device,dtype=torch.float).unsqueeze(1)
 
 class policy(nn.Module):
     def __init__(self):
         super().__init__()
-        self.c1 = nn.LazyConv2d(64,1,1)
-        self.c2 = nn.LazyConv2d(64,1,1)
-        self.c3 = nn.LazyConv2d(64,1,1)
+        self.c1 = nn.LazyConv2d(32,1,1,0)
+        self.c2 = nn.LazyConv2d(64,2,2,0)
+        self.c3 = nn.LazyConv2d(64,2,2,0)
+        self.c4 = nn.LazyConv2d(64,2,2,0)
+        self.l1 = nn.LazyLinear(5120)
+        self.l2 = nn.LazyLinear(2048)
+        self.l3 = nn.LazyLinear(512)
+        self.l4 = nn.LazyLinear(12)
         self.vaux = nn.LazyLinear(1)
 
     def forward(self,x):
-        pass
+        x = F.silu(self.c1(x))
+        x = F.silu(self.c2(x))
+        x = F.silu(self.c3(x))
+        x = F.silu(self.c4(x))
+        x = F.silu(self.l1(x.flatten(1)))
+        x = F.silu(self.l2(x))
+        x = F.silu(self.l3(x))
+        return F.sigmoid(self.l4(x)), self.vaux(x) 
 
+class value(nn.module):
+    def __init__(self):
+        self.c1 = nn.LazyConv2d(16,4,2,1)  
+        self.c2 = nn.LazyConv2d(32,4,2,1)
+        self.c3 = nn.LazyConv2d(32,3,2,1)
+        self.l1 = nn.LazyLinear(256)
+        self.l2 = nn.LazyLinear(128)
+        self.l3 = nn.LazyLinear(1)
+        
+    def forward(self, x):
+        x = F.silu(self.c1(x))
+        x = F.silu(self.c2(x))
+        x = F.silu(self.c3(x))
+        x = F.silu(self.l1(x.flatten(1)))
+        x = F.silu(self.l2(x))
+        return self.l3(x)
 
-
-    
